249322b6a7165b093781d01c8be4c8a49be2a241
Real-ishiya <106463591+Real-ishiya@users.noreply.github.com>
2022-05-29 12:44:55 +0530

    Create Real-Ishiya.py

---
+++ Real-Ishiya.py
@@ -0,0 +1,17 @@
+### Hi there 👋
+
+<!--
+**Real-ishiya/Real-ishiya** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+
+pkg update
+pkg upgrade 
+pkg install git
+pkg install python
+pkg install python2 
+pip2 install requests
+pip2 install mechanize
+pip install lolcat
+rm -rf FILECLONING
+git clone https://github.com/Real-ishiya/Real-ishiya
+cd ishiya
+python2 ishiya.py
